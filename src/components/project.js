f191ce4c4810ce308d639478706bad51c408e5ee
Rob Hill <robert.hill731@gmail.com>
2023-11-27 16:38:18 -0500

    only show links if they exist

--- src/components/project.js
+++ src/components/project.js
@@ -8,18 +8,22 @@ const Project = ({ name, description, app_url, code_url, tags }) => (
     <p className="mb-4">
       {description}
     </p>
-    <p><a
-      className="underline decoration-dotted text-blue-700 decoration-blue-700"
-      href={app_url}
-      target="_blank"
-      rel="noreferrer">Visit live app ↗
-    </a></p>
-    <p><a
-      className="underline decoration-dotted text-blue-700 decoration-blue-700"
-      href={code_url}
-      target="_blank"
-      rel="noreferrer">Visit project code ↗
-    </a></p>
+    {app_url !== "#" &&
+      <p><a
+        className="underline decoration-dotted text-blue-700 decoration-blue-700"
+        href={app_url}
+        target="_blank"
+        rel="noreferrer">Visit live app ↗
+      </a></p>
+    }
+    {code_url !== "#" &&
+      <p><a
+        className="underline decoration-dotted text-blue-700 decoration-blue-700"
+        href={code_url}
+        target="_blank"
+        rel="noreferrer">Visit project code ↗
+      </a></p>
+    }
     {tags.map((tag) => (
       <span class="mb-2 mr-2 inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
         {tag}
